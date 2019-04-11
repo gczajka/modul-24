726393763b87412bec1eb83265735a6643719146
gczajka <gczajkajv@gmail.com>
2019-04-11 19:09:29 +0200

    zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calc = new Calculator();
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        int result = calc.add(1,2);
+        if(result == 3) {
+            System.out.println("Method add works");
+        } else {
+            System.out.println("Error in method add");
+        }
+
+        System.out.println("Test - drugi test jednostkowy:");
+
+        int result1 = calc.subtract(5,2);
+        if(result1 == 3) {
+            System.out.println("Method subtract works");
+        } else {
+            System.out.println("Error in method subtract");
+        }
     }
 }
